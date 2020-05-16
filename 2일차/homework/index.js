a990f7b8b4d188b635a57222952882971548cc5d
DP_Bell <49894647+mjmoon608@users.noreply.github.com>
2020-05-16 19:01:10 +0900

    homework index.html, js
    2일차 숙제 오류 수정해야함.

---
+++ 2일차/homework/index.js
@@ -0,0 +1,6 @@
+function order_product() {
+  let customerName = $("#customer-name").val();
+  let customNum = $("#customer-num").val();
+  let customerAdress = $("#customer-adress").val();
+  let customerTel = $("#customer-tel").val();
+}
